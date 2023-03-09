b39965e6140b0d27450d2ab5e1511a7a95740f1e
kevin <kevin9000m@Gmail.com>
2023-03-09 16:44:28 +0000

    Add JUnit test cases for the findLargest() function to test order and boundaries, negative numbers, a single number, and duplicates.

--- src/test/kotlin/Maintest.kt
+++ src/test/kotlin/Maintest.kt
@@ -8,5 +8,16 @@ class Maintest {
         assertEquals(9, findLargest(intArrayOf(7, 8, 9)))
 
     }
-
+    @Test
+    fun testNegativeNumbers() {
+        assertEquals(-7, findLargest(intArrayOf(-7,-8,-9)))
+    }
+    @Test
+    fun testForOnlyOneNumber() {
+        assertEquals(7, findLargest(intArrayOf(7)))
+    }
+    @Test
+    fun testDuplicates() {
+        assertEquals(9, findLargest(intArrayOf(9, 7, 9, 8)))
+    }
 }
